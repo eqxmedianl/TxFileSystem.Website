c4ff1f0c01c907e60690eac07a50554173101130
Jarno Kamminga <jarno.kamminga@eqx-media.nl>
2021-04-13 14:22:21 +0200

    Added a GitHub link to the footer

--- Website/ClientApp/src/components/Footer.js
+++ Website/ClientApp/src/components/Footer.js
@@ -20,7 +20,8 @@ export class Footer extends Component {
         return (
             <footer class="footer border-top w-100 p-2">
                 <div class="container text-center">
-                    <span class="text-muted">&copy; 2021 <a href="https://www.eqx-media.nl/" target="_blank">EQX Media B.V.</a> All rights reserved.</span>
+                    <span class="text-muted">&copy; 2021 <a href="https://www.eqx-media.nl/" target="_blank">EQX Media B.V.</a> All rights reserved.</span> -
+                    <span>View the source <a href="https://github.com/eqxmedianl/TxFileSystem.Website" target="_blank">on GitHub.</a></span>
                 </div>
             </footer>
         );
