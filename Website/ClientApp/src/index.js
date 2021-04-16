b571f8939fb2741fd8b86067f0cc8863eae1c397
Jarno Kamminga <jarno.kamminga@eqx-media.nl>
2021-04-16 23:37:53 +0200

    Changed to BSD license too, as per TxFileSystem

--- Website/ClientApp/src/index.js
+++ Website/ClientApp/src/index.js
@@ -1,12 +1,9 @@
 /**
  *
- * The code is this file is subject to EQX Proprietary License. Therefor it is copyrighted and restricted
- * from being copied, reproduced or redistributed by any party or indiviual other than the original
- * copyright holder mentioned below.
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the conditions mentioned in the shipped license are met.
  *
- * It's also not allowed to copy or redistribute the compiled binaries without explicit consent.
- *
- * (c) 2021 EQX Media B.V. - All rights are stricly reserved.
+ * Copyright (c) 2021, EQX Media B.V. - All rights reserved.
  *
  */
 import 'bootstrap/dist/css/bootstrap.css';
