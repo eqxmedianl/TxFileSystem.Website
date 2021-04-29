eb4dd1c12aef423bd0e3162451147e8d8f695539
Jarno Kamminga <jarno.kamminga@eqx-media.nl>
2021-04-29 14:09:48 +0200

    Change footer text regarding project website license

--- Website/ClientApp/src/components/Layout/Footer.js
+++ Website/ClientApp/src/components/Layout/Footer.js
@@ -17,8 +17,8 @@ export class Footer extends Component {
             <footer className="page-footer footer border-top w-100 p-2 bg-dark">
                 <div className="container text-center">
                     <span className="text-muted d-block d-md-inline">&copy; 2021 <a className="text-muted footer-link" href="https://www.eqx-media.nl/" target="_blank" rel="noopener noreferrer">EQX Media B.V.</a> All rights reserved.</span><span className="d-none d-md-inline text-muted">&nbsp;&#9733;&nbsp;</span>
-                    <span className="text-muted d-block d-md-inline">Read the <a className="text-muted footer-link" href="/license">EQX&nbsp;Proprietary&nbsp;License</a>.</span><span className="d-none d-md-inline text-muted">&nbsp;&#9733;&nbsp;</span>
-                    <span className="text-muted d-block d-md-inline">View the proprietary <a className="text-muted footer-link" href="https://github.com/eqxmedianl/TxFileSystem.Website" target="_blank" rel="noopener noreferrer">source on GitHub.</a></span>
+                    <span className="text-muted d-block d-md-inline">Read the <a className="text-muted footer-link" href="/license">License</a>.</span><span className="d-none d-md-inline text-muted">&nbsp;&#9733;&nbsp;</span>
+                    <span className="text-muted d-block d-md-inline">View the <a className="text-muted footer-link" href="https://github.com/eqxmedianl/TxFileSystem.Website" target="_blank" rel="noopener noreferrer">source on GitHub.</a></span>
                 </div>
             </footer>
         );
